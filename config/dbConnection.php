5ca36d3ccc3ff61b28339422930b360abb906eda
break <breaker@wo.cn>
2017-03-26 14:46:35 +0800

    Update dbConnection.php

--- config/dbConnection.php
+++ config/dbConnection.php
@@ -9,15 +9,15 @@
 use Workerman\Worker;
 
 //ip地址
-$ip='192.168.2.128';
+$ip='0.0.0.0';
 //端口
 $port='3306';
 //用户名
-$user='root';
+$user='name';
 //密码
-$password='Admin123';
+$password='pass';
 //数据库
-$database='workerman';
+$database='databasename';
 //new一个数据库连接的实例
 $db = new Workerman\MySQL\Connection($ip,$port,$user,$password,$database);
 // 将db实例存储在全局变量中
